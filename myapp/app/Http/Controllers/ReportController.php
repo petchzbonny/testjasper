475b26bd25657d7ae36cdccea20bb598ced33982
PETCHBONNY <bonnyjung004@gmail.com>
2026-05-06 14:32:23 +0700

    update ล่าสุด

--- myapp/app/Http/Controllers/ReportController.php
+++ myapp/app/Http/Controllers/ReportController.php
@@ -3,55 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Process;
 
 class ReportController extends Controller
 {
- public function openPdf(Request $request)
-{
-    $jasperStarter = 'C:\\jasperstarter\\bin\\jasperstarter.exe';
-    $reportFile = storage_path('app/reports/Blank_A4.jrxml');
-    $outputDir = storage_path('app/reports/output');
-    $outputPdf = $outputDir . DIRECTORY_SEPARATOR . 'Blank_A4.pdf';
+    public function openPdf(Request $request)
+    {
+        $jasperStarter = 'C:\\Program Files (x86)\\JasperStarter\\bin\\jasperstarter.exe';
+        $reportFile = storage_path('app/reports/products_report.jrxml');
+        $outputDir = storage_path('app/reports/output');
+        $outputPdf = $outputDir . DIRECTORY_SEPARATOR . 'products_report.pdf';
 
-    if (!file_exists($jasperStarter)) {
-        dd('ไม่พบ jasperstarter.exe', $jasperStarter);
+        if (!file_exists($jasperStarter)) {
+            abort(500, 'ไม่พบ jasperstarter.exe');
+        }
+
+        if (!file_exists($reportFile)) {
+            abort(500, 'ไม่พบไฟล์ report');
+        }
+
+        if (!is_dir($outputDir)) {
+            mkdir($outputDir, 0777, true);
+        }
+
+        if (file_exists($outputPdf)) {
+            unlink($outputPdf);
+        }
+
+        $cmd = '"C:\\Program Files (x86)\\JasperStarter\\bin\\jasperstarter.exe"'
+            . ' pr "' . $reportFile . '"'
+            . ' -f pdf'
+            . ' --jdbc-dir "C:\\jdbc"'
+            . ' -t mysql'
+            . ' -H 127.0.0.1'
+            . ' --db-port 3306'
+            . ' -n webportal'
+            . ' -u root'
+            . ' -o "' . $outputDir . '"'
+            . ' 2>&1';
+
+        shell_exec($cmd);
+
+        if (!file_exists($outputPdf)) {
+            abort(500, 'ไม่สามารถสร้าง PDF ได้');
+        }
+
+        return response()->file($outputPdf, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="products_report.pdf"',
+        ]);
     }
-
-    if (!file_exists($reportFile)) {
-        dd('ไม่พบไฟล์ report', $reportFile);
-    }
-
-    if (!is_dir($outputDir)) {
-        mkdir($outputDir, 0777, true);
-    }
-
-    if (file_exists($outputPdf)) {
-        unlink($outputPdf);
-    }
-
-    $process = new Process([
-        $jasperStarter,
-        'pr',
-        $reportFile,
-        '-f', 'pdf',
-        '-t', 'mysql',
-        '-H', '127.0.0.1',
-        '-n', 'webportal',
-        '-u', 'root',
-        '--db-port', '3306',
-        '-o', $outputDir,
-    ]);
-
-    $process->setTimeout(120);
-    $process->run();
-
-    dd([
-        'successful' => $process->isSuccessful(),
-        'output' => $process->getOutput(),
-        'errorOutput' => $process->getErrorOutput(),
-        'outputPdfExists' => file_exists($outputPdf),
-        'outputPdf' => $outputPdf,
-    ]);
-}
 }
